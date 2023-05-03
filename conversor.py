d16e2e557a35b067d6813f7b6c4af6dd7a85a881
JavierAlbertoBenitez <javieralbertobenitez97@gmail.com>
2023-05-03 14:31:06 -0300

    Se deja un solo programa el que pasa de una base a otra

--- conversor.py
+++ conversor.py
@@ -1,11 +1,13 @@
 import os
 
+seguir = True
+
+base2 = {"10": "A", "11": "B", "12": "C", "13": "D", "14": "E", "15": "F"}
+
 base = ["0", "1", "2", "3", "4", "5", "6", "7",
         "8", "9", "A", "B", "C", "D", "E", "F"]
 
 letras = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
-
-seguir = True
 
 
 def basenADec(num, bas):
@@ -21,33 +23,46 @@ def basenADec(num, bas):
     return dec
 
 
-def convertir(b):
+def decABasen(num, bas):
+    bin = ''
+    while num // bas != 0:
+        aux = num % bas
+        if aux < 10:
+            bin = str(aux)+bin
+        else:
+            bin = base[str(aux)]+bin
+        num //= bas
+    return str(num) + bin
+
+
+def iniciar():
+    try:
+        b = int(input("ingrese base inicial (2 a 16): "))
+        assert b > 1 and b < 17
+        baseAConvertir(b)
+    except (ValueError, AssertionError):
+        print('Ingrese base valida')
+
+
+def baseAConvertir(b):
+    try:
+        c = int(input("ingrese base a convertir (2 a 16): "))
+        assert c > 1 and c < 17
+        convertir(b, c)
+    except (ValueError, AssertionError):
+        print('Ingrese base valida')
+
+
+def convertir(b, c):
     try:
         n = str(input(f"Ingresa el numero en base {b}: "))
         for x in n:
             if x not in base[:b]:
                 raise ValueError
-        print(f"El numero en base DEC es {basenADec(n, b)}")
+            d = basenADec(n, b)
+        print(f"El número en base {c} es: {decABasen(d,c)}")
     except (ValueError):
         print('Ingrese numeros validos para dicha base')
-
-
-def iniciar():
-    try:
-        b = int(input('Ingrese base (de 2 a 16): '))
-        if b < 2 or b > 16:
-            raise ValueError
-        convertir(b)
-    except (ValueError, AssertionError):
-        print('Ingrese una base valida')
-
-
-def seguir():
-    r = str(input('Presionar Y para continuar de lo contrario otra letra: '))
-    if r == "Y":
-        os.system("cls")  # os.system("clear") fuera de android
-    else:
-        continuar = False
 
 
 def continuar():
